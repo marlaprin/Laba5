4267141e12d5b7cb349ca1e5cb5c7b49aa63e0b1
eterb <eterb@DESKTOP-K5UKVPS>
2022-09-24 14:20:28 +0700

    Добавьте файлы проекта.

---
+++ Laba5.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+double x = Convert.ToDouble(Console.ReadLine());
+double y = Convert.ToDouble(Console.ReadLine());
+const int r = 1;
+// x^2 + y^2 = r^2;
+// x = Math.Sqrt(r**r - y**y);
+// y = Math.Sqrt(r**r - x**x);
+if ((x >= 0) && (y >= 0) && (x*x + y*y <= r*r))
+{
+    Console.WriteLine("Точка с координатами O(x; y) принадлежит второй четверти единичной окружности.");
+}
+else
+{
+    Console.WriteLine("Точка с координатами O(x; y) не принадлежит второй четверти единичной окружности!");
+}
---
+++ Laba5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+Random rnd = new Random(DateTime.Now.Millisecond);
+int[] a = new int[100];
+int count = 0;
+int sum = 0;
+for (int i = 0; i < 100; i++)
+{
+    a[i] = rnd.Next(-1001, 1001);
+    if (a[i] % 2 == 0)
+    {
+        count += a[i];
+        Console.WriteLine("Количество чётных чисел в массиве ->", count);
+    }
+    if (a[i] % 3 == 0)
+    {
+        count += a[i];
+        Console.WriteLine("Количество чисел кратных 3-ём в массиве ->", count);
+    }
+
+    if (a[i] < 0)
+    {
+        count += a[i];
+        sum += a[i];
+    }
+    else
+    {
+        Console.WriteLine("Eror!");
+    }
+}
+Console.WriteLine("Количество нечётных в массиве чисел:", count, "и сумма нечётных в массиве чисел:", sum);
+
+
+
